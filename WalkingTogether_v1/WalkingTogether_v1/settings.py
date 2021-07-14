090c1848ec3c929ce1b2eb55a6f389eff3dcc12b
YoungDaLee <ldy0956@gmail.com>
2021-07-14 20:55:46 +0900

    Set settings

--- WalkingTogether_v1/WalkingTogether_v1/settings.py
+++ WalkingTogether_v1/WalkingTogether_v1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ssg(2%w0@@g!h74rry4&gmt%sn6rkoxt^+i-*oj=11^3^m1c_'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
