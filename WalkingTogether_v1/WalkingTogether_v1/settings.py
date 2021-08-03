7a2cd46cb769cccfb7d7c5ad157a742b142f6787
YoungDaLee <ldy0956@gmail.com>
2021-08-03 22:31:20 +0900

    MOD : migrations

--- WalkingTogether_v1/WalkingTogether_v1/settings.py
+++ WalkingTogether_v1/WalkingTogether_v1/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS +=[
 # Django-Rest-Framework 설정
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
