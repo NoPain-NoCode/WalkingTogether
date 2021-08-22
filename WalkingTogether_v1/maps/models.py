1a3df846be9d29ae334ce879d71c97c707dc3f0a
YoungDaLee <ldy0956@gmail.com>
2021-08-22 20:14:49 +0900

    DEL : image field

--- WalkingTogether_v1/maps/models.py
+++ WalkingTogether_v1/maps/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
 from django.db import models
+# from django.contrib.auth.models import User
+# from user.models import User
 
 # Create your models here.
 class WalkingTrails(models.Model):
@@ -17,18 +18,27 @@ class WalkingTrails(models.Model):
     point_name = models.CharField(max_length=255,verbose_name='포인트명칭')
     longitude = models.DecimalField(decimal_places=14,max_digits=17,verbose_name='경도')
     latitude  = models.DecimalField(decimal_places=14,max_digits=16,verbose_name='위도')
+    
+    # count_review = models.IntegerField(default=0, verbose_name="리뷰 개수")
+
+    def __str__(self):
+        return str(self.point_number)
+
     class Meta:
         managed = False
         db_table = 'walkingtrails'
+        verbose_name = 'Walking Trails' 
+        verbose_name_plural = 'Walking Trails'  # 복수형 지정
 
-class Review():
-    id = models.IntegerField(primary_key=True,verbose_name="리뷰 id")
-    point_id = models.ForeignKey(WalkingTrails,related_name="walkingtrails", on_delete=models.CASCADE, db_column='point_id', verbose_name="산책로 id")
-    user_id = models.ForeignKey(User,related_name="user", on_delete=models.CASCADE, db_column='user_id', verbose_name="User id")
+class Review(models.Model):
+    # id = models.CharField(max_length=255,primary_key=True,verbose_name="리뷰 id")
+    walkingtrails = models.ForeignKey(WalkingTrails,blank=True, null=True, related_name="walkingtrails", db_column="point_number", on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User',blank=True, null=True, related_name="user", on_delete=models.CASCADE)
     content = models.TextField(null=True, verbose_name="내용")
+    
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="작성시간")
     updated_date = models.DateTimeField(auto_now=True, verbose_name="수정 시간")
-    image = models.ImageField(null=True, verbose_name="이미지")
+    # image = models.ImageField(blank=True,null=True, verbose_name="이미지")
     
     # 별점 선택지
     REVIEW_POINT_CHOICES = (
@@ -41,18 +51,21 @@ class Review():
 
     # 강아지 산책 가능 여부
     DOG_POSS=(
-    ('가능'),
-    ('불가능'),
+    ('ok','가능해요'),
+    ('no','불가능해요'),
+    ('dontKnow','잘 모르겠어요')
     )
 
-    point = models.IntegerField(choices=REVIEW_POINT_CHOICES,verbose_name="별점")
-    dog_possible = models.CharField(blank=True, null=True, choices=DOG_POSS, verbose_name="강아지 산책 가능 여부")
+    point = models.CharField(max_length=255,blank=True,choices=REVIEW_POINT_CHOICES,verbose_name="별점")
+    dog_possible = models.CharField(max_length=255,blank=True, null=True, choices=DOG_POSS, verbose_name="강아지 산책 가능 여부")
 
 
     objects = models.Manager()
 
     def __str__(self):
-        pass
+        return str(self.id)
+    
     class Meta:
-        managed = False
         db_table = 'review'
+        verbose_name = 'review'
+        verbose_name_plural = 'reviews'
